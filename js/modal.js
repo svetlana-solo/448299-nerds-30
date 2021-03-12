0425215bae44f0eac2498623268a39637f6e2063
svetlana-solo <to_s@list.ru>
2021-03-12 18:32:58 +0700

    завершение сетки

---
+++ js/modal.js
@@ -0,0 +1,56 @@
+const loginFeedback = document.querySelector(".button-feedback");
+const loginPopup = document.querySelector(".modal");
+const loginClose = loginPopup.querySelector(".modal-close");
+const loginForm = loginPopup.querySelector(".feedback-form");
+const loginLogin = loginPopup.querySelector(".login-user");
+const loginPassword = loginPopup.querySelector(".login-email-user");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginFeedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginPassword.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginPassword.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+      loginPopup.classList.remove("modal-error");
+    }
+  }
+});
